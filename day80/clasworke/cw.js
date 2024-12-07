e57382d7ac6212c6e18adae2b1a0cca2d2415bf1
Dima-chad <muxigulashvilidimitri@gmail.com>
2024-12-07 23:28:14 +0400

    Delete day80 directory

--- day80/clasworke/cw.js
+++
@@ -1,5 +0,0 @@
-
-    let name1 = prompt("შეიყვანეთ პირველი ადამიანის სახელი:");
-    let name2 = prompt("შეიყვანეთ მეორე ადამიანის სახელი:")
-    let love = Math.floor(Math.random() * 101)
-      alert(`${name1} და ${name2} სიყვარულის პროცენტი არის ${love}%`);
